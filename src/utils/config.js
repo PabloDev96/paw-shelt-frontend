37cd61234d1b272f1ae07af2890b6433836fb326
PabloDev96 <pablo.diazgar@gmail.com>
2025-08-21 09:53:14 +0200

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,2 @@
-const API_URL = import.meta.env.VITE_API_URL?.replace(/\/+$/, "") || "";
-console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
-console.log("API_URL (normalizado) =", API_URL);
-export { API_URL };
+export const API_URL =
+  import.meta.env.VITE_API_URL?.replace(/\/+$/, "") || "https://paw-shelt.onrender.com";
