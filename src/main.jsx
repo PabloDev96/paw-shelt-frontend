7f23a35107b1659f367b34108b1c77aa5c0bcfdb
PabloDev96 <pablo.diazgar@gmail.com>
2025-08-21 10:01:31 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@fullcalendar/daygrid/index.css';
-import '@fullcalendar/timegrid/index.css';
+import '@fullcalendar/common/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
+import '@fullcalendar/list/main.css';
+
 import './index.css'
 import App from './App.jsx'
 
